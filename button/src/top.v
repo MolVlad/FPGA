7bf0fde7d49039e290d382647e8ec3d4bcfd0a0f
molvlad <molvlad@frtk.ru>
2019-03-09 21:35:47 +0300

    pinout config

--- button/src/top.v
+++ button/src/top.v
@@ -1,14 +1,14 @@
 module top(
     input CLK,
-	 input S3,
+	 input S1,
 
-    output DS_C,
+    output DS_A, DS_B, DS_C, DS_D,
 	 output DS_EN1, DS_EN2, DS_EN3, DS_EN4
 );
 
 assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = 4'b1111;
 
-assign DS_C = S3;
+assign DS_A = S1;
 
 endmodule
 
