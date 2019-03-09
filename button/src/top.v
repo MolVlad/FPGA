229ebb45eb8b0cc1b03e4b9b22255ebbfb7214bb
molvlad <molvlad@frtk.ru>
2019-03-09 23:23:25 +0300

    lool

--- button/src/top.v
+++ button/src/top.v
@@ -8,12 +8,11 @@ module top(
 
 assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = 4'b1111;
 
-clk_div #(.x(6)) clk_div(.clk(clk), .clk_out(divided_clk));
+clk_div #(.x(6)) clk_div(.clk(CLK), .clk_out(divided_clk));
 
 button_bounce button_bounce(.clk(divided_clk), .button(S2), .state(button_state));
 
 assign DS_C = button_state;
-//assign DS_C = S2;
 
 endmodule
 
