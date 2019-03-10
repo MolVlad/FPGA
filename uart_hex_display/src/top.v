75d8f5d279ef5a04ba75ae07175f7a957026f4ad
molvlad <molvlad@frtk.ru>
2019-03-10 13:14:18 +0300

    it works

--- uart_hex_display/src/top.v
+++ uart_hex_display/src/top.v
@@ -6,19 +6,14 @@ module top(
     output DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G, DS_DP
 );
 
-wire bounce_protect_clk;
-
-clk_div #(.x(18), .y(625)) clk_div(.clk(CLK), .clk_out(bounce_protect_clk));	// 625 * 8 = 5000; 5000 * 9600 = 48.000.000
-
-wire value;
 wire uart_clk;
 
-bounce_protect bounce_protect(.clk(bounce_protect_clk), .in(TXD), .out(value), .clk_out(uart_clk));
+clk_div #(.x(18), .y(1250)) clk_div(.clk(CLK), .clk_out(uart_clk));	// 1250 * 38400 = 48.000.000
 
 wire [7:0]uart_data;
 wire flag_complete;
 
-uart_in uart_in(.clk(uart_clk), .in(value), .data(uart_data[7:0]), .flag_complete(flag_complete));
+uart_in uart_in(.clk(uart_clk), .in(TXD), .data(uart_data), .flag_complete(flag_complete));
 
 reg [15:0]data = 16'hAA00;
 
