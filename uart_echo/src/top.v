9398dc87367f1db1db8a421a9195d66f58484e1e
molvlad <molvlad@frtk.ru>
2019-03-11 08:52:27 +0300

    it works

--- uart_echo/src/top.v
+++ uart_echo/src/top.v
@@ -8,14 +8,20 @@ module top(
     output DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G, DS_DP
 );
 
+/* -------------- Clock ------------------ */
+
 wire uart_clk;
 
 custome_clk_div #(.x(18), .y(1250)) custome_clk_div(.clk(CLK), .clk_out(uart_clk));	// 1250 * 38400 = 48.000.000
+
+/* -------------- UART TX ---------------- */
 
 wire [7:0]uart_data;
 wire flag_complete;
 
 uart_in uart_in(.clk(uart_clk), .in(TXD), .data(uart_data), .flag_complete(flag_complete));
+
+/* -------------- Display ---------------- */
 
 wire [15:0]data;
 
@@ -33,14 +39,16 @@ assign {DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G, DS_DP} = seg;
 
 hex_display hex_display(.clk(CLK), .data(data), .anodes(anodes), .seg(seg));
 
-wire button_state;
+/* -------------- UART RX ---------------- */
 
-button_bounce button_bounce(.clk(CLK), .button(S2), .state(button_state));
+wire [7:0]ascii;
 
-wire button_edge;
+hex_to_ascii hex_to_ascii(.hex(data[3:0]), .ascii(ascii));
 
-posedge_impulse posedge_impulse(.clk(uart_clk), .in(~button_state), .out(button_edge));
+wire uart_in_edge;
 
-uart_queue uart_queue(.clk(uart_clk), .data(data), .flag_start(button_edge), .out(RXD));
+posedge_impulse posedge_impulse(.clk(uart_clk), .in(flag_complete), .out(uart_in_edge));
+
+uart_out uart_out(.clk(uart_clk), .data(ascii), .out(RXD), .flag_start(uart_in_edge));
 
 endmodule
